7782988cda0734c7cbde6d2d9881187efb6569ad
Yuri <yuripetusko@gmail.com>
2021-11-10 09:53:00 +0000

    return default systemPropertres

--- packages/core/src/hooks/use-system-properties.ts
+++ packages/core/src/hooks/use-system-properties.ts
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
 import { ISystemProperties } from '../types/system-properties';
 import { SubstraHooksContext } from '../providers/substrahooks-provider/context';
+import { systemPropertiesDefaults } from '../helpers';
 
 export const useSystemProperties = (apiProviderId?: string): ISystemProperties => {
   const id = apiProviderId || useContext(SubstraHooksContext).defaultApiProviderId;
-  return useContext(SubstraHooksContext).apiProviders[id]?.systemProperties;
+  return (
+    useContext(SubstraHooksContext).apiProviders[id]?.systemProperties || systemPropertiesDefaults
+  );
 };
