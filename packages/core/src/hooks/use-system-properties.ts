76f45e7d91f56f102317dc344c7e50e6b78af5f3
Yuri <yuripetusko@gmail.com>
2021-11-10 09:52:01 +0000

    Update use-system-properties.ts

--- packages/core/src/hooks/use-system-properties.ts
+++ packages/core/src/hooks/use-system-properties.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { ISystemProperties } from '../types/system-properties';
 import { SubstraHooksContext } from '../providers/substrahooks-provider/context';
 
-export const useSystemProperties = (apiProviderId?: string): ISystemProperties | null => {
+export const useSystemProperties = (apiProviderId?: string): ISystemProperties => {
   const id = apiProviderId || useContext(SubstraHooksContext).defaultApiProviderId;
-  return useContext(SubstraHooksContext).apiProviders[id]?.systemProperties || null;
+  return useContext(SubstraHooksContext).apiProviders[id]?.systemProperties;
 };
