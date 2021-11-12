0507fc8021d9c8238a0f99f363f1abede9764c0f
Yuri <yuripetusko@gmail.com>
2021-11-12 12:13:45 +0000

    Return default api provider object

--- packages/core/src/hooks/use-api-provider.ts
+++ packages/core/src/hooks/use-api-provider.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { SubstraHooksContext } from '../providers/substrahooks-provider/context';
 import { ApiPromise } from '@polkadot/api';
 
-export const useApiProvider = (apiProviderId?: string): ApiPromise => {
+export const useApiProvider = (apiProviderId?: string): ApiPromise | undefined => {
   const id = apiProviderId || useContext(SubstraHooksContext).defaultApiProviderId;
   return useContext(SubstraHooksContext).apiProviders[id]?.apiProvider;
 };
