ddf0d4e90b82a017dd825e37c41deed6f789f7d2
Yuri <yuripetusko@gmail.com>
2021-11-12 12:21:39 +0000

    Update use-api-provider.ts

--- packages/core/src/hooks/use-api-provider.ts
+++ packages/core/src/hooks/use-api-provider.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { SubstraHooksContext } from '../providers/substrahooks-provider/context';
 import { ApiPromise } from '@polkadot/api';
 
-export const useApiProvider = (apiProviderId?: string): ApiPromise | undefined => {
+export const useApiProvider = (apiProviderId?: string): ApiPromise | null => {
   const id = apiProviderId || useContext(SubstraHooksContext).defaultApiProviderId;
   return useContext(SubstraHooksContext).apiProviders[id]?.apiProvider;
 };
