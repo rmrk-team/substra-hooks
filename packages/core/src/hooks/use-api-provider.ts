833542544042f0f9f3bdcc685f16da77ba3660fe
Yuri <yuripetusko@gmail.com>
2021-11-12 16:23:45 +0000

    make sure hook is never called conditionly

--- packages/core/src/hooks/use-api-provider.ts
+++ packages/core/src/hooks/use-api-provider.ts
@@ -3,6 +3,6 @@ import { SubstraHooksContext } from '../providers/substrahooks-provider/context'
 import { ApiPromise } from '@polkadot/api';
 
 export const useApiProvider = (apiProviderId?: string): ApiPromise | null => {
-  const id = apiProviderId || useContext(SubstraHooksContext).defaultApiProviderId;
-  return useContext(SubstraHooksContext).apiProviders[id]?.apiProvider;
+  const defaultId = useContext(SubstraHooksContext).defaultApiProviderId;
+  return useContext(SubstraHooksContext).apiProviders[apiProviderId || defaultId]?.apiProvider;
 };
