05b7c95d7662b8f286e97cbd44217452ba4f78db
Yuri <yuripetusko@gmail.com>
2021-11-15 22:12:15 +0000

    Export getEncodedAddress util

--- packages/core/src/helpers/index.ts
+++ packages/core/src/helpers/index.ts
@@ -2,3 +2,4 @@ export * from './get-account-balance';
 export * from './get-asset-balance';
 export * from './fetch-system-properties';
 export * from './format-price';
+export * from './get-encoded-address';
