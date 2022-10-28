30f86dc6f14ec182b359ccd1a71b6193f343a672
Yuri <yuripetusko@gmail.com>
2022-10-28 17:22:12 +0100

    use useEffectOnce

--- packages/core/src/helpers/use-is-mounted-ref.ts
+++ packages/core/src/helpers/use-is-mounted-ref.ts
@@ -1,13 +1,14 @@
 import { useEffect, useRef } from 'react';
+import { useEffectOnce } from './use-effect-once';
 
 export const useIsMountedRef = () => {
   const isMountedRef = useRef<null | boolean>(null);
-  useEffect(() => {
+  useEffectOnce(() => {
     isMountedRef.current = true;
     return () => {
       isMountedRef.current = false;
     };
-  }, []);
+  });
 
   return isMountedRef;
 };
